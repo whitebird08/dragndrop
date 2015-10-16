0193753cfb8f331c3e9f5d76a82e8102c840f70f
Ryan Bird <ryan01bird@gmail.com>
2015-10-16 10:51:41 -0600

    added droppable

---
+++ public/javascripts/droppable.js
@@ -0,0 +1,25 @@
+//moved to accordian js
+
+// $(function() {
+//     $( "#tasks" ).accordion();
+//     $( "#tasks li" ).draggable({
+//       appendTo: "body",
+//       helper: "clone"
+//     });
+//     $( "#plan ol" ).droppable({
+//       activeClass: "ui-state-default",
+//       hoverClass: "ui-state-hover",
+//       accept: ":not(.ui-sortable-helper)",
+//       drop: function( event, ui ) {
+//         $( this ).find( ".placeholder" ).remove();
+//         $( "<li></li>" ).text( ui.draggable.text() ).appendTo( this );
+//       }
+//     }).sortable({
+//       items: "li:not(.placeholder)",
+//       sort: function() {
+//         // gets added unintentionally by droppable interacting with sortable
+//         // using connectWithSortable fixes this, but doesn't allow you to customize active/hoverClass options
+//         $( this ).removeClass( "ui-state-default" );
+//       }
+//     });
+//   });
