e890cb89331e7048effe5fb79c67c64c5730a0f8
Ryan Bird <ryan01bird@gmail.com>
2015-10-16 12:52:41 -0600

    added db and query with partial jade loop

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var db = require('monk')('localhost/dragndrop');
+var taskCollection = db.get('tasks');
 
 /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Drag n Drop' });
+// });
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Drag n Drop' });
+  taskCollection.find({}, function(err, data){
+    console.log(data, 'DATAAAAAAA')
+    res.render('index', {allCategories: data });
+  })
+
 });
 
 module.exports = router;
